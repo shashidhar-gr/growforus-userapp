e206fc42d43be5cd61c02af3c3f18dde916e5549
shashidhar-gr <shashidhargr26@gmail.com>
2018-06-12 20:51:57 +0530

    Added endpoint for registration

--- client/screens/Registration.js
+++ client/screens/Registration.js
@@ -23,6 +23,23 @@ export class Registration extends Component {
         this.setState({ isReady: true });
     }
     
+    registerUser(url, data) {
+        return fetch(url, {
+            body: JSON.stringify(data), // must match 'Content-Type' header
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, same-origin, *omit
+            headers: {
+              'user-agent': 'Mozilla/4.0 MDN Example',
+              'content-type': 'application/json'
+            },
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, cors, *same-origin
+            redirect: 'follow', // manual, *follow, error
+            referrer: 'no-referrer', // *client, no-referrer
+          })
+          .then(response => response.json()) // parses response to JSON
+    }
+
     render() {
         if (!this.state.isReady) {
             return <Expo.AppLoading />;
@@ -45,7 +62,18 @@ export class Registration extends Component {
                         </Item>
                         <Button primary block onPress={
                             () => {
-                                onSignIn().then(() => this.props.navigation.navigate("SignedIn"));
+
+                                this.registerUser('http://192.168.43.239:3000/users/register',
+                                {
+                                    "username": "shashidhar",
+                                    "password": "mypassword"
+                                })
+                                .then(
+                                    data => console.log(data)
+                                ) // JSON from `response.json()` call
+                                .catch(
+                                    error => console.error(error)
+                                )
                             }
                         }>
                             <Text> Register </Text>
